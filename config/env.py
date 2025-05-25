5da041a3dadad694e282d3dd883eec69ef8ec77c
BLauf <nlaufer@calpoly.edu>
2025-05-25 16:16:26 -0700

    got rid of salesforce env

--- config/env.py
+++ config/env.py
@@ -8,9 +8,6 @@ def get_env_variables():
     return {
         "aws_access_key": os.getenv("AWS_ACCESS_KEY_ID"),
         "aws_secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-        "sf_username": os.getenv("SF_USERNAME"),
-        "sf_password": os.getenv("SF_PASSWORD"),
-        "sf_security_token": os.getenv("SF_SECURITY_TOKEN"),
         "tavily_api_key": os.getenv('TAVILY_API_KEY')
     }
 
